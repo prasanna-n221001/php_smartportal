487aa27ced99227cd84ae93718b02fbb63bb2a81
prasanna-n221001 <n221001@rguktn.ac.in@college.edu>
2026-02-18 11:57:24 +0000

    successfully added login with google

--- login.php
+++ login.php
@@ -1,15 +1,50 @@
 <?php
 include "database.php";
 
-$email = $_POST['email'];
-$password = $_POST['password'];
+<?php
+$client_id = "YOUR_CLIENT_ID";
+$redirect_uri = "http://localhost/itsmypage/oauth2.0/google_auth.php";
+$scope = "email profile";
 
-$query = "SELECT * FROM users WHERE email='$email' AND password='$password'";
+$google_login_url = "https://accounts.google.com/o/oauth2/v2/auth?" . http_build_query([
+    'client_id' => $client_id,
+    'redirect_uri' => $redirect_uri,
+    'response_type' => 'code',
+    'scope' => $scope,
+    'access_type' => 'offline',
+    'prompt' => 'consent'
+]);
+?>
+
+
+$email = trim($_POST['email']);
+$password = trim($_POST['password']);
+
+
+
+$query = "SELECT * FROM users WHERE email='$email'";
 $result = mysqli_query($conn, $query);
 
 if (mysqli_num_rows($result) == 1) {
-    echo "Login Successful ";
+    echo "Email found in database<br>";
+    $row = mysqli_fetch_assoc($result);
+    if ($password === $row['password']) {
+        echo "Login successful";
+    } else {
+        echo " Password mismatch";
+    }
+
 } else {
-    print "Invalid Email or Password ";
+    echo " Email not found";
 }
 ?>
+<html>
+    <body>
+        <hr>
+<h3>OR</h3>
+
+<a href="<?= $google_login_url ?>">
+    <button>Login with Google</button>
+</a>
+</body>
+</html>
